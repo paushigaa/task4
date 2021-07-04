711bb63ff8ce0b8181609f8b4de74ea638506d33
paushigaa <86842437+paushigaa@users.noreply.github.com>
2021-07-04 19:09:41 +0530

    task4 of mycaptain
    created by paushi from mycaptain

---
+++ mycapex3.py
@@ -0,0 +1,16 @@
+list1=[]
+n=int(input("enter the number of elements in list1: "))
+for i in range(0,n):
+    ele1=int(input("enter the element for list1: "))
+    list1.append(ele1)
+for j in list1:
+    if j>0:
+        print(j)
+list2=[]
+n=int(input("enter the number of elements in list2: "))
+for i in range(0,n):
+    ele2=int(input("enter the element for list2: "))
+    list2.append(ele2)
+for k in list2:
+    if k>0:
+        print(k)
